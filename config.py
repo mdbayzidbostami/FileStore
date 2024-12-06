84b404b934ba4ea2c096880e89d61b91586dfa2a
mdbayzidbostami <bayzid.simran@gmail.com>
2024-12-06 09:42:52 +0600

    Update config.py

--- config.py
+++ config.py
@@ -31,8 +31,8 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002172515270')) #Log channel id ( make sure bot is admin )
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001473043276"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1001644866777"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001960183584"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002333397554"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
